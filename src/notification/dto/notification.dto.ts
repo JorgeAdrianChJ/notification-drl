9dfb3d8c876aca22354cadd3ff458573fa7c67b0
Jorge Chavarria <jorge.chavarria@gamanza.com>
2020-09-25 11:30:25 -0600

    commit to master

--- src/notification/dto/notification.dto.ts
+++ src/notification/dto/notification.dto.ts
@@ -1,11 +1,28 @@
-import { Status } from '../enum/status.enum';
 export class Notificacion {
-  Status: Status;
-  EventId: number;
-  Mcc: number;
-  Mnc: string;
-  Msgcount: number;
-  Msisdn: string;
-  Received: number;
-  UserReference: string;
+  messageId: string;
+  smsId: string;
+  campaignTrackingId: string;
+  part: number;
+  parts: number;
+  label: string;
+  to: string;
+  from: string;
+  country: string;
+  operator: string;
+  groups: [string];
+  campaignName: string;
+  status: {
+    name: string;
+    reason: {
+      detailedStatus: string;
+      description: string;
+    };
+    updatedDate: string;
+  };
+  message: string;
+  applicationName: string;
+  latency: number;
+  price: number;
+  direction: string;
+  originatingService: string;
 }
