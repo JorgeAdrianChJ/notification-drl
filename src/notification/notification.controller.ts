fec8396ef9bd63342ee5476f13db64cac2d8aa36
JorgeAdrianChJ <38114049+JorgeAdrianChJ@users.noreply.github.com>
2023-09-05 18:05:40 -0600

    Update notification.controller.ts

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -5,8 +5,9 @@ import { Notificacion } from './dto/notification.dto';
 export class NotificationController {
   private logger = new Logger('NotificationController');
   @Post()
-  AddNotificacion(@Body() notification: any): Notificacion {
-    this.logger.log(notification);
+  AddNotificacion(@Body() notification: any,@Headers() headers): Notificacion {
+    this.logger.log("Headers:",headers);
+    this.logger.log("Event:"notification);
     return notification;
   }
 }
