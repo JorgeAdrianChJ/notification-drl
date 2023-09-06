7a2d20be1807bf87195cb91dca605eef089b6351
JorgeAdrianChJ <38114049+JorgeAdrianChJ@users.noreply.github.com>
2023-09-05 18:13:53 -0600

    Update notification.controller.ts

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -6,8 +6,8 @@ export class NotificationController {
   private logger = new Logger('NotificationController');
   @Post()
   AddNotificacion(@Body() notification: any,@Headers() headers): Notificacion {
-    this.logger.log("Headers:",headers);
-    this.logger.log("Event:",notification);
+    console.log("Headers:",headers);
+    console.log("Event:",notification);
     return notification;
   }
 }
