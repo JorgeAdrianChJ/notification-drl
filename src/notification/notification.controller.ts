4d1b38b32cc90226d1f8e2d171a521d3e3bd357e
JorgeAdrianChJ <38114049+JorgeAdrianChJ@users.noreply.github.com>
2023-09-05 18:20:05 -0600

    Update notification.controller.ts

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Body, Post,Headers, Logger } from '@nestjs/common';
+import { Controller, Body, Post,Headers, Logger,Req } from '@nestjs/common';
 import { Notificacion } from './dto/notification.dto';
 
 @Controller('notification')
 export class NotificationController {
   private logger = new Logger('NotificationController');
   @Post()
-  AddNotificacion(@Body() notification: any,@Headers() headers): Notificacion {
+  AddNotificacion(@Body() notification: any,@Headers() headers: any,@Req() request: any): Notificacion {
+    console.log("Rest:",rest);
     console.log("Headers:",headers);
     console.log("Event:",notification);
     return notification;
